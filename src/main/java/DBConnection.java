6f15d0561510a417a6c0fd0f47e7a22e05f21fdb
nealgoogs <ngeoghegan@my.ccsu.edu>
2024-10-22 20:05:25 -0400

    Unneccessary code, cleaning up

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -5,10 +5,11 @@ import java.sql.SQLException;
 public class DBConnection {
 
     // Method to create a connection to MySQL
+    // Don't think we need this cuz of Lambda?
+    // Just random method for now if we need it
     public Connection createConnection() throws SQLException {
         String jdbcUrl = "jdbc:mysql://database-2.cp6y8k80ib5l.us-east-1.rds.amazonaws.com:3306/database-2s";
-        String username = "admin";
-        String password = "123asdzxc";
-        return DriverManager.getConnection(jdbcUrl, username, password);
+
+        return DriverManager.getConnection(jdbcUrl);
     }
 }
